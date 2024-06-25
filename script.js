0be5e6396a9fc9b0eddabaf734d48e44f3b578e4
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-06-25 21:01:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var num1 =+ prompt("enter first num");
+var num2 =+ prompt("enter Second num");
+var num3 =+ prompt("enter third num");
+
+var num4=(num1+num2+num3)
+alert(num4)
+
+
+var num4=(num1-num2-num3)
+alert(num4)
+
+var num4=(num1*num2*num3)
+alert(num4)
+
+var num4=(num1/num2/num3)
+alert(num4)
+
